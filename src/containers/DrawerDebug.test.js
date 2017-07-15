0844077cf52332249bc016791edc4c2cda666e32
Ben Lu <ben@ben-l.com>
2017-07-16 11:12:36 +1200

    Added drawer debug test

---
+++ src/containers/DrawerDebug.test.js
@@ -0,0 +1,21 @@
+import 'react-native'
+import React from 'react'
+import {Provider} from 'react-redux'
+import DrawerDebug from './DrawerDebug'
+import configureStore from '../store'
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer'
+
+const store = configureStore()
+
+it('renders correctly', ()=>{
+  const props = {
+  }
+  const tree = renderer.create(
+    <Provider store={store}>
+      <DrawerDebug {...props} />
+    </Provider>
+  )
+  expect(tree).toMatchSnapshot()
+})
